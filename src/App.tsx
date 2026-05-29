45c8d305ae4de37a70bee5f29774a60c5ba4b2fd
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-05-29 12:30:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const AppShell: React.FC = () => {
     <div className="app-shell">
       <ScrollToTop />
 
-      {!isCleanProductRoute ? <WatermarkLayer /> : null}
+      {/*!isCleanProductRoute ? <WatermarkLayer /> : null */}
 
       <Header />
 
