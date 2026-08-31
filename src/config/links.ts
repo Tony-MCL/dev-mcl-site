4c6c9ff461ce130acf2da6481310bd4b1f1be03b
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-31 11:19:50 +0200

    Add Fury O product page

--- src/config/links.ts
+++ src/config/links.ts
@@ -7,4 +7,6 @@ export const LINKS = {
   husketGooglePlay: "https://play.google.com/store/apps/details?id=no.morningcoffeelabs.husket&pcampaignid=web_share",
   findbackGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.findback",
   findbackAppStore: "https://apps.apple.com/us/app/findback-onetap/id6796313821",
+  furyGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.furyo",
+  furyAppStore: "https://apps.apple.com/us/app/fury-o/id6798349790",
 } as const;
