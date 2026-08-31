d8f94dc49955f0deab9be985588e4d042bf97160
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-31 11:06:51 +0200

    Add FindBack product page

--- src/config/links.ts
+++ src/config/links.ts
@@ -5,4 +5,6 @@ export const LINKS = {
   kvittekGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.kvittek",
   kvittekAppStore: "",
   husketGooglePlay: "https://play.google.com/store/apps/details?id=no.morningcoffeelabs.husket&pcampaignid=web_share",
+  findbackGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.findback",
+  findbackAppStore: "https://apps.apple.com/us/app/findback-onetap/id6796313821",
 } as const;
