42742ba3f096fd8086c03af31aff11c94f7b7b5d
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-31 10:50:01 +0200

    Support simplified husk'et product page

--- src/config/links.ts
+++ src/config/links.ts
@@ -4,4 +4,5 @@ export const LINKS = {
   progress: "https://progress.managesystem.no",
   kvittekGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.kvittek",
   kvittekAppStore: "",
+  husketGooglePlay: "https://play.google.com/store/apps/details?id=no.morningcoffeelabs.husket&pcampaignid=web_share",
 } as const;
